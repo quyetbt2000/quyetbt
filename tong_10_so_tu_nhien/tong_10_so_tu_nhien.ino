758143625a4c27a3a3d70f365f6b762e7f895088
quyetbt2000 <quyetbt2000@gmail.com>
2019-09-20 12:57:49 +0700

    quyetbt

---
+++ tong_10_so_tu_nhien/tong_10_so_tu_nhien.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+int i=0;
+for (i=0;i<10;i++);
+delay(500);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
